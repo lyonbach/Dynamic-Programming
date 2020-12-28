dfa2f6309ce147586ddd14216325ecff208899e3
Bahadir Arslan <arslan.bah@gmail.com>
2020-12-28 22:28:55 +0100

    Updated best_sum.py with the tabulation edition.

--- best_sum.py
+++ best_sum.py
@@ -1,8 +1,9 @@
 """
-Python implementation of the code for the fifth chapter.
+Python implementation of the fifth and the thirteenth chapters code.
 Original videos can be viewed from the following link:
 https://www.youtube.com/watch?v=oBt53YbR9Kk
 """
+
 
 import copy
 
@@ -32,13 +33,14 @@ def best_sum(target_sum: int, numbers: list):
 
     return shortest_result
 
-def mem_best_sum(target_sum: int, numbers: list, memo: dict={}):
+def mem_best_sum(target_sum: int, numbers: list, memo: dict=None):
 
     """
     Returns a list containing "shortest" combination of elements that add up to exactly as input target_sum.
     Returns None if target_sum can not be reached.
     """
 
+    memo = {} if memo is None else memo
     if target_sum in memo:
         return memo[target_sum]
 
@@ -64,3 +66,25 @@ def mem_best_sum(target_sum: int, numbers: list, memo: dict={}):
 
 # Notice slightly modified code at lines 54, 58 and 60.
 # This is because Python is passing lists by reference and we could not be exactly same as in the series.
+
+def tab_best_sum(target_sum: int, numbers: list):
+
+    """
+    Returns a list containing "shortest" combination of elements that add up to exactly as input target_sum.
+    Returns None if target_sum can not be reached.
+    """
+
+    table = [None for _ in range(target_sum + 1)]
+    table[0] = []
+
+    for idx in range(target_sum + 1):
+        for number in numbers:
+            if idx + number > target_sum:
+                break
+            if table[idx] is None:
+                continue
+
+            current_list = list(table[idx]) + [number] # We need to copy the list, not change it.
+            if table[idx + number] is None or len(table[idx + number]) > len(current_list):
+                 table[idx + number] = current_list
+    return table[-1]
