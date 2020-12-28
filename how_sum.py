1694d35ae318cfc518f601f2edc36810489d2e17
Bahadir Arslan <arslan.bah@gmail.com>
2020-12-28 13:28:06 +0100

    Updated how_sum.py with the tabulation edition.

--- how_sum.py
+++ how_sum.py
@@ -1,9 +1,10 @@
 """
-Python implementation of the code for the fourth chapter.
+Python implementation of the fourth and the twelfth chapters code.
 Original videos can be viewed from the following link:
 https://www.youtube.com/watch?v=oBt53YbR9Kk
 """
 
+# Fourth Chapter
 def how_sum(target_sum: int, numbers: list):
 
     """
@@ -26,12 +27,14 @@ def how_sum(target_sum: int, numbers: list):
 
     return None
 
-def mem_how_sum(target_sum: int, numbers: list, memo: dict):
+def mem_how_sum(target_sum: int, numbers: list, memo: dict=None):
 
     """
     Returns a list containing any combination of elements that add up to exactly as input target_sum.
     Returns None if target_sum can not be reached.
     """
+
+    memo = {} if memo is None else memo
 
     if target_sum in memo:
         return memo[target_sum]
@@ -51,3 +54,26 @@ def mem_how_sum(target_sum: int, numbers: list, memo: dict):
             return remainder_result
     memo.update({target_sum: None})
     return None
+
+
+# Twelfth Chapter
+def tab_how_sum(target_sum: int, numbers: list):
+
+    """
+    Returns a list containing any combination of elements that add up to exactly as input target_sum.
+    Returns None if target_sum can not be reached.
+    """
+
+    table = [None for _ in range(target_sum + 1)]
+    table[0] = []
+
+    for idx in range(target_sum + 1):
+        for number in numbers:
+            if idx + number > target_sum:
+                break
+            if table[idx] is None:
+                continue
+            if table[idx + number] is None:
+                table[idx + number] = list(table[idx]) + [number] # We need to copy the list, not change it.
+
+    return table[target_sum]
